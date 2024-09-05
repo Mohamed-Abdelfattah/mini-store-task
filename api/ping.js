cd1da7a997814f19dd3d9d5ebc3ac7bd0afc1fc0
Mohamed-Abdelfattah <45965447+Mohamed-Abdelfattah@users.noreply.github.com>
2024-09-05 19:25:38 +0300

    converting edge function to serverless function

--- api/ping.js
+++ api/ping.js
@@ -1,22 +1,12 @@
-// /api/ping.js
-export const config = {
-    runtime: 'edge',
-  };
-  
-  export default async function handler(req) {
-    try {
-      const response = await fetch('https://mini-store-task-tan.vercel.app/products?category=all');
-      const status = response.status;
-      console.log(`Pinged website. Status: ${status}`);
-      return new Response(JSON.stringify({ message: 'Ping successful', status }), {
-        status: 200,
-        headers: { 'Content-Type': 'application/json' },
-      });
-    } catch (error) {
-      console.error('Ping failed:', error);
-      return new Response(JSON.stringify({ message: 'Ping failed', error: error.message }), {
-        status: 500,
-        headers: { 'Content-Type': 'application/json' },
-      });
-    }
+// api/ping.js
+export default async function handler(req, res) {
+  try {
+    const response = await fetch('https://mini-store-task-tan.vercel.app/products?category=all');
+    const status = response.status;
+    console.log(`Pinged website. Status: ${status}`);
+    res.status(200).json({ message: 'Ping successful', status });
+  } catch (error) {
+    console.error('Ping failed:', error);
+    res.status(500).json({ message: 'Ping failed', error: error.message });
   }
+}
