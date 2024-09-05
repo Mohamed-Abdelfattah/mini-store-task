58678dbdd3b218d50a3c0f1537d4732c006b11be
Mohamed-Abdelfattah <45965447+Mohamed-Abdelfattah@users.noreply.github.com>
2024-09-05 19:32:34 +0300

    fixing serverless function

--- api/ping.js
+++ api/ping.js
@@ -1,12 +1,23 @@
 // api/ping.js
+const https = require('https');
+
 export default async function handler(req, res) {
-  try {
-    const response = await fetch('https://mini-store-task-tan.vercel.app/products?category=all');
-    const status = response.status;
-    console.log(`Pinged website. Status: ${status}`);
-    res.status(200).json({ message: 'Ping successful', status });
-  } catch (error) {
+  const url = 'https://mini-store-task-tan.vercel.app/products?category=all';
+
+  https.get(url, (response) => {
+    let data = '';
+
+    response.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    response.on('end', () => {
+      console.log(`Pinged website. Status: ${response.statusCode}`);
+      res.status(200).json({ message: 'Ping successful', status: response.statusCode });
+    });
+
+  }).on('error', (error) => {
     console.error('Ping failed:', error);
     res.status(500).json({ message: 'Ping failed', error: error.message });
-  }
+  });
 }
